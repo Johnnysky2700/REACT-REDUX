ceb5fa8b5dbd2dca7ce0e5126b4e57bd0cdc064b
iamjohnnysky <vanpasie20@gmail.com>
2025-03-31 15:20:21 -0700

    React&Redux Conditional Output

--- myapp/src/Johnnysky.js
+++ myapp/src/Johnnysky.js
@@ -1,18 +1,32 @@
 import React, { Component } from 'react';
 
 const Johnnysky = ({johnnysky}) => {
-        const johnnyList = johnnysky.map(johnny => {
-            return (
-                <div className='johnnysky' key={johnny.id}>
-                   <div>Name: { johnny.name }</div>
-                   <div>Age: { johnny.age }</div>
-                   <div>Belt: { johnny.belt }</div>
-                </div>
-            )
-    })
+    // const johnnyList = johnnysky.map(johnny => {
+    //     if (johnny.age > 20){
+    //         return (
+    //             <div className='johnnysky' key={johnny.id}>
+    //                 <div>Name: { johnny.name }</div>
+    //                 <div>Age: { johnny.age }</div>
+    //                 <div>Belt: { johnny.belt }</div>
+    //                 </div>
+    //             )
+    //     }else {
+    //         return null
+    //     }    
+    // })
     return(
         <div className="johnny-list">
-            { johnnyList }
+            { 
+                johnnysky.map(johnny => {
+                    return johnny.age > 20 ? (
+                        <div className='johnnysky' key={johnny.id}>
+                            <div>Name: { johnny.name }</div>
+                            <div>Age: { johnny.age }</div>
+                            <div>Belt: { johnny.belt }</div>
+                        </div>
+                    ) : null;
+                })
+            }
             </div>
     )
 }
