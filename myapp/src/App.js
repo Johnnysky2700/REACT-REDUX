f80f603312c640d82d1691ec542d5136fd222f06
iamjohnnysky <vanpasie20@gmail.com>
2025-04-02 05:31:27 -0700

    React&Redux Lifecycle Methods

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -25,6 +25,13 @@ class App extends Component {
         johnnysky: johnnysky
       })
   }
+  componentDidMount(){
+    console.log('component mounted')
+  }
+  componentDidUpdate(prevProps, prevState){
+    console.log('component update');
+    console.log(prevProps, prevState)
+  }
   render() {
     return (
       <div className="App">
