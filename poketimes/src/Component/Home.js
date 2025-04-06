23b744ff9ea25358ecf1e0c211ac73fe0808f97a
iamjohnnysky <vanpasie20@gmail.com>
2025-04-06 14:05:19 -0700

    React&Redux Mapping State to Props

--- poketimes/src/Component/Home.js
+++ poketimes/src/Component/Home.js
@@ -1,21 +1,10 @@
-import React, { useState, useEffect } from "react";
-import axios from "axios";
-import { Link } from 'react-router-dom'
-import Pokeball from '../pokeball.png'
+import React from "react";
+import { Link } from 'react-router-dom';
+import Pokeball from '../pokeball.png';
+import { connect } from 'react-redux';
 
-const Home = () => {
-    const [posts, setPosts] = useState([]); // ✅ Use state to store posts
-
-    useEffect(() => {
-        axios.get("https://jsonplaceholder.typicode.com/posts")
-            .then(res => {
-                console.log("API Response:", res.data);
-                setPosts(res.data.slice(0, 10)); // ✅ Set the first 10 posts
-            })
-            .catch(err => {
-                console.error("Error fetching posts:", err);
-            });
-    }, []); // ✅ Runs only once when the component mounts
+const Home = (props) => {
+    const { posts } = props;
 
     return (
         <div className="container home">
@@ -26,7 +15,7 @@ const Home = () => {
                         <img src={Pokeball} alt="A pokeball" />
                         <div className="card-content">
                             <Link to={'/' + post.id}>
-                            <span className="card-title red-text">{post.title}</span>
+                                <span className="card-title red-text">{post.title}</span>
                             </Link>
                             <p>{post.body}</p>
                         </div>
@@ -39,4 +28,10 @@ const Home = () => {
     );
 };
 
-export default Home;
+const mapStateToProps = (state) => {
+    return {
+        posts: state.posts
+    };
+};
+
+export default connect(mapStateToProps)(Home);
--- poketimes/src/Reducer/rootReducer.js
+++ poketimes/src/Reducer/rootReducer.js
@@ -1,9 +1,26 @@
 const initState = {
-    posts: []
-}
-
-const rootReducer = (state, action) => {
+    posts: [
+      {
+        id: '1',
+        title: 'Squirtle Laid an Egg',
+        body: 'lorem ipsum, dolor sit amet conrum, voluptatum iste laboharum perferendis,enim repellendus ipsam sunt autem at odit dolorum, voluptatum suscipi iste harum  cum magni itaque animi laudantium fugiat'
+      },
+      {
+        id: '2',
+        title: 'Charmander Laid an Egg',
+        body: 'lorem ipsum, dolor sit amet conrum, voluptatum iste laboharum perferendis,enim repellendus ipsam sunt autem at odit dolorum, voluptatum suscipi iste harum  cum magni itaque animi laudantium fugiat'
+      },
+      {
+        id: '3',
+        title: 'A Helix Fossil was Found',
+        body: 'lorem ipsum, dolor sit amet conrum, voluptatum iste laboharum perferendis,enim repellendus ipsam sunt autem at odit dolorum, voluptatum suscipi iste harum cum magni itaque animi laudantium fugiat'
+      }
+    ]
+  };
+  
+  const rootReducer = (state = initState, action) => {
     return state;
-}
-
-export default rootReducer
+  };
+  
+  export default rootReducer;
+  
