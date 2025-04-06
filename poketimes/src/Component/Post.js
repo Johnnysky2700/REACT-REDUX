a56fb07e5cb5b12101c5feab50331f4c0c18672c
iamjohnnysky <vanpasie20@gmail.com>
2025-04-06 14:56:46 -0700

    React&Redux Map Dispatch to props

--- poketimes/src/Component/Post.js
+++ poketimes/src/Component/Post.js
@@ -1,10 +1,18 @@
 import React from 'react';
-import { useParams } from 'react-router-dom';
-import { useSelector } from 'react-redux';
+import { useParams, useNavigate } from 'react-router-dom';
+import { useSelector, useDispatch } from 'react-redux';
 
 const Post = () => {
-    const { post_id } = useParams(); // grabs the ID from the URL
+    const { post_id } = useParams();
+    const navigate = useNavigate();
+    const dispatch = useDispatch();
+
     const post = useSelector(state => state.posts.find(p => p.id === post_id));
+
+    const handleClick = () => {
+        dispatch({ type: 'DELETE_POST', id: post_id });
+        navigate('/');
+    };
 
     return (
         <div className="container">
@@ -12,6 +20,11 @@ const Post = () => {
                 <div className="post">
                     <h4 className="center">{post.title}</h4>
                     <p>{post.body}</p>
+                    <div className="center">
+                        <button className="btn grey" onClick={handleClick}>
+                            Delete post
+                        </button>
+                    </div>
                 </div>
             ) : (
                 <div className="center">Loading post...</div>
